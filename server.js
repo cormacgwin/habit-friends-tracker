42b4cf2968dd00f6b050c9ee2f515a7bb4100546
cormacgwin <cormacgoodwin@gmail.com>
2025-10-22 22:49:19 -0400

    Update server to use routes

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Import route modules
+const userRoutes = require('./routes/users');
+const habitRoutes = require('./routes/habits');
+const competitionRoutes = require('./routes/competitions');
+
+// Mount routes
+app.use('/users', userRoutes);
+app.use('/habits', habitRoutes);
+app.use('/competitions', competitionRoutes);
+
+// Root route
 app.get('/', (req, res) => {
   res.send('Habit Tracker API');
-});
-
-// placeholder for getting all habits
-app.get('/habits', (req, res) => {
-  res.json([]);
 });
 
 app.listen(port, () => {
